d6f93cc6e6bf041e37533e6caf62e86974ab3510
medharik <med.harik@gmail.com>
2023-11-10 11:18:14 +0100

    form add to mysql

--- seance8/bd.php
+++ seance8/bd.php
@@ -3,8 +3,11 @@
 // mysql, -=>obsolete, + => facile
 // mysqli,oci,mssql, sql_srv -=> SECURITE, MONO SGBDR , +=> facile 
 // pdo : - => difficile, necessite une connaissance de la POO. +=> SECURITE, MULTI-SGBDR  
+$libelle=$_POST['libelle'];
+$prix=$_POST['prix'];
 $connection=mysqli_connect("localhost","root","","dbase") ;
 // $connection2=mysqli_connect("localhost","root","","dbasefhfhg");
-mysqli_query($connection,"insert into produit(libelle,prix) values('acer a7',7500)") ;
+mysqli_query($connection,"insert into produit(libelle,prix) values('$libelle',$prix)") ;
 
 ?>
+<h1>Produit ajoute avec succes</h1>
